fa358907449ecdf3386b8a0b1710a489a670367b
ymzeng1 <83317731+ymzeng1@users.noreply.github.com>
2021-05-05 17:57:40 -0500

    Update code.py

--- Data Structure/LinkedList/链表中倒数第k个结点/code.py
+++ Data Structure/LinkedList/链表中倒数第k个结点/code.py
@@ -1,1 +1,15 @@
+# 快慢指针
+# 先让快指针走k步，然后让快慢指针一起走，直到快指针到尽头
+class Solution:
+    def getKthFromEnd(self, head: ListNode, k: int) -> ListNode:
+        front = head
+        behind = head
+        while k > 0:
+            front = front.next
+            k-=1
 
+        while front:
+            front = front.next
+            behind = behind.next
+
+        return behind

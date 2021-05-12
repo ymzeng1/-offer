faeec18cf7668f169c3d85905e6df32829ac3ef7
ymzeng1 <83317731+ymzeng1@users.noreply.github.com>
2021-05-11 23:57:03 -0500

    Create code.py

---
+++ Data Structure/Tree（二叉树/对称的二叉树/code.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isSymmetric(self, root: TreeNode) -> bool:
+        def recur(L, R):
+            if not L and not R: return True
+            if not L or not R or L.val != R.val: return False
+            return recur(L.left, R.right) and recur(L.right, R.left)
+
+        return recur(root.left, root.right) if root else True
+

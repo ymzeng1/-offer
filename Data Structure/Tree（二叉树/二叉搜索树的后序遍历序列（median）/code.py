8b835838f26b3d49eb7c2c5b1edab1026878d478
ymzeng1 <83317731+ymzeng1@users.noreply.github.com>
2021-05-12 15:52:34 -0500

    Create code.py

---
+++ Data Structure/Tree（二叉树/二叉搜索树的后序遍历序列（median）/code.py
@@ -0,0 +1,12 @@
+# 分治递归
+class Solution:
+    def verifyPostorder(self, postorder: [int]) -> bool:
+        def recur(i, j):
+            if i >= j: return True
+            p = i
+            while postorder[p] < postorder[j]: p += 1
+            m = p
+            while postorder[p] > postorder[j]: p += 1
+            return p == j and recur(i, m - 1) and recur(m, j - 1)
+
+        return recur(0, len(postorder) - 1)

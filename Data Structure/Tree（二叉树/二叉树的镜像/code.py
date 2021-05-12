2976ae4207b0aa1892e68b0b8b2da82dc001467f
ymzeng1 <83317731+ymzeng1@users.noreply.github.com>
2021-05-11 23:23:31 -0500

    Update code.py

--- Data Structure/Tree（二叉树/二叉树的镜像/code.py
+++ Data Structure/Tree（二叉树/二叉树的镜像/code.py
@@ -1,3 +1,4 @@
+#递归
 class Solution:
     def mirrorTree(self, root: TreeNode) -> TreeNode:
         if not root: return None
@@ -5,3 +6,6 @@ class Solution:
         return root
 
 #Python 利用平行赋值的写法（即 a, b = b, aa,b=b,a ），可省略暂存操作。其原理是先将等号右侧打包成元组 (b,a)(b,a), 再序列地分给等号左侧的 a,b 序列。
+
+
+#辅助栈（队列）

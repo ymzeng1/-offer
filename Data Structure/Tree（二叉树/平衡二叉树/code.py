09a2ad541e59ddeac4bd736a5aaafa0363dcd3f6
ymzeng1 <83317731+ymzeng1@users.noreply.github.com>
2021-05-13 00:18:59 -0500

    Create code.py

--- Data Structure/Tree（二叉树/平衡二叉树/code.py
+++ Data Structure/Tree（二叉树/平衡二叉树/code.py
@@ -8,3 +8,17 @@ class Solution:
     def depth(self, root):
         if not root: return 0
         return max(self.depth(root.left), self.depth(root.right)) + 1
+
+    
+# 后序遍历 + 剪枝 （最优）目前还没看懂
+class Solution:
+    def isBalanced(self, root: TreeNode) -> bool:
+        def recur(root):
+            if not root: return 0
+            left = recur(root.left)
+            if left == -1: return -1
+            right = recur(root.right)
+            if right == -1: return -1
+            return max(left, right) + 1 if abs(left - right) <= 1 else -1
+
+        return recur(root) != -1

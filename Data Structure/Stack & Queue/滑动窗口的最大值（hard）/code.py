c81b9ac0cb7d8ccf2383ca6a8e6fd03d51b1bdbe
ymzeng1 <83317731+ymzeng1@users.noreply.github.com>
2021-05-14 13:46:48 -0500

    Update code.py

--- Data Structure/Stack & Queue/滑动窗口的最大值（hard）/code.py
+++ Data Structure/Stack & Queue/滑动窗口的最大值（hard）/code.py
@@ -7,3 +7,19 @@ class Solution:
             res.append(max(nums[i:i+k]))
             i += 1
         return res
+
+    
+        # 单调队列（单调递减）    
+        if not nums: return []   
+        res = []
+        queue = collections.deque()
+        for i in range(k):
+            while queue and queue[-1] < nums[i]: queue.pop() # 删除所有比num[i]小的数
+            queue.append(nums[i])
+        res.append(queue[0])
+        for i in range(k, len(nums)):
+            if nums[i-k] == queue[0]: queue.popleft()
+            while queue and queue[-1] < nums[i]: queue.pop()
+            queue.append(nums[i])
+            res.append(queue[0])
+        return res

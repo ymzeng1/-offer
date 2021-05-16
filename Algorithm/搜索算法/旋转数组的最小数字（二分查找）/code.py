5ea8915fec10d2c152405f30d0d0ebe3e69bb691
ymzeng1 <83317731+ymzeng1@users.noreply.github.com>
2021-05-16 18:26:30 -0500

    Create code.py

---
+++ Algorithm/搜索算法/旋转数组的最小数字（二分查找）/code.py
@@ -0,0 +1,9 @@
+class Solution:
+    def minArray(self, numbers: List[int]) -> int:
+        i, j = 0, len(numbers)-1
+        while i < j:
+            mid = (i+j)//2
+            if numbers[mid] > numbers[j]: i = mid + 1
+            elif numbers[mid] < numbers[j]: j = mid
+            else: j -= 1
+        return numbers[i]

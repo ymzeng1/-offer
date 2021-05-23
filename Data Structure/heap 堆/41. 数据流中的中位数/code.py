984b456e49f74b4941e74dd2e17152917ba7cade
ymzeng1 <83317731+ymzeng1@users.noreply.github.com>
2021-05-23 17:07:49 -0500

    Update code.py

--- Data Structure/heap 堆/41. 数据流中的中位数/code.py
+++ Data Structure/heap 堆/41. 数据流中的中位数/code.py
@@ -1,1 +1,22 @@
-a
+class MedianFinder:
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.minh = []
+        self.maxh = []
+
+
+    def addNum(self, num: int) -> None:
+        if len(self.minh) == len(self.maxh):
+            heapq.heappush(self.minh, -heapq.heappushpop(self.maxh, -num))
+        else:
+            heapq.heappush(self.maxh, -heapq.heappushpop(self.minh, num))
+
+
+    def findMedian(self) -> float:
+        if len(self.minh) != len(self.maxh):
+            return self.minh[0]
+        else: 
+            return (self.minh[0] - self.maxh[0]) / 2.0

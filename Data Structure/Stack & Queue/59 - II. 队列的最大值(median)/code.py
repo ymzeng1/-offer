9203b4bbf2bcb3e49a7dd368f2db5b5a4ab1711e
ymzeng1 <83317731+ymzeng1@users.noreply.github.com>
2021-05-26 15:18:15 -0500

    Create code.py

---
+++ Data Structure/Stack & Queue/59 - II. 队列的最大值(median)/code.py
@@ -0,0 +1,22 @@
+import queue
+
+class MaxQueue:
+    def __init__(self):
+        self.queue = queue.Queue()
+        self.deque = queue.deque()
+
+    def max_value(self) -> int:
+        return self.deque[0] if self.deque else -1
+
+    def push_back(self, value: int) -> None:
+        self.queue.put(value)
+        while self.deque and self.deque[-1] < value:
+            self.deque.pop()
+        self.deque.append(value)
+
+    def pop_front(self) -> int:
+        if self.queue.empty(): return -1
+        val = self.queue.get()
+        if val == self.deque[0]:
+            self.deque.popleft()
+        return val

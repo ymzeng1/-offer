7a17ed6ed26927f52d86eb6909ed6555e20d5251
ymzeng1 <83317731+ymzeng1@users.noreply.github.com>
2021-06-24 18:03:25 -0500

    Delete code.py

--- Algorithm/位运算/65. 不用加减乘除做加法/code.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def constructArr(self, a: List[int]) -> List[int]:
-        left, right = [1]*len(a), [1]*len(a)
-        for i in range(1, len(a)):
-            left[i] = left[i-1] * a[i-1]
-            right[len(a)-1-i] = right[len(a)-i] * a[len(a)-i]
-        return [a*b for a, b in zip(left, right)]
